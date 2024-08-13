c9762f759d0a02205245b3ef63a888fd84291fb5
yonnii12 <yonnii401@hotmail.com>
2024-08-13 15:21:07 -0500

    Menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("        *****************************************************\n" +
+                           "        *       CAPITULO/GLOSARIO PROGRAMACION BASICA       *\n" +
+                           "        *                 YVILL13-CMENES1                   *\n" +
+                           "        *                   PRE-HACKATON                    *\n" +
+                           "        *****************************************************\n" +
+                           "        *       1.  Datos Primitivos.                       *\n" +
+                           "        *       2.  String.                                 *\n" +
+                           "        *       3.  Constantes.                             *\n" +
+                           "        *       4.  Tipo de Operadores.                     *\n" +
+                           "        *       5.  Condicional IF, ELSE IF ELSE.           *\n" +
+                           "        *       6.  Condicional Switch.                     *\n" +
+                           "        *       7.  Condicional Ternaria.                   *\n" +
+                           "        *       8.  Bucle DO WHILE.                         *\n" +
+                           "        *       9.  Bucle WHILE.                            *\n" +
+                           "        *       10. Bucle FOR.                              *\n" +
+                           "        *       0.  Salir.                                  *\n" +
+                           "        *****************************************************");
         System.out.println("cmenes1");
     }
 }
