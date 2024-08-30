dee3c66a26e46c6c193c794aeacbd065f3789e2e
yonnii12 <yonnii401@hotmail.com>
2024-08-29 22:17:46 -0500

    Finalizacion punto 5 if, if else y else

--- src/Main.java
+++ src/Main.java
@@ -491,9 +491,136 @@ public class Main {
                     break;
 
                 case 5:
+                    do {
+                        System.out.println("\n");
+                        System.out.println("        *****************************************************");
+                        System.out.println("        *                   CONDICIONALES                   *");
+                        System.out.println("        *                 IF, ELSE IF y ELSE                *");
+                        System.out.println("        *****************************************************");
+                        System.out.println("        *                                                   *");
+                        System.out.println("        *       1.  Explicacion IF.                         *");
+                        System.out.println("        *       2.  Explicacion ELSE IF.                    *");
+                        System.out.println("        *       3.  Explicacion ELSE.                       *");
+                        System.out.println("        *       4.  Programa IF, ELSE IF, ELSE.             *");
+                        System.out.println("        *       5.  Atras.                                  *");
+                        System.out.println("        *                                                   *");
+                        System.out.println("        *****************************************************");
+                        System.out.print("\n Ingrese el número de la opción que desea ver: ");
+                        opcionSubMenu = scanner.nextInt();
+                        switch (opcionSubMenu) {
+
+                            case 1:
+                                System.out.println("\n");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                   EXPLICACION IF                  *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *   En Java, una instrucción if se utiliza          *");
+                                System.out.println("        *   para tomar decisiones en el flujo de            *");
+                                System.out.println("        *   ejecución de un programa. Permite ejecutar un   *");
+                                System.out.println("        *   bloque de código solo si se cumple una          *");
+                                System.out.println("        *   condición específica.                           *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *           Su estructura es la siguiente:          *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *    int x = 10;                                    *");
+                                System.out.println("        *    if (x > 5) {                                   *");
+                                System.out.println("        *        System.out.println(\"x es mayor que 5\");  *");
+                                System.out.println("        *             }                                     *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("\n Ingrese Enter para  volver al menu anterior");
+                                scanner.nextLine();
+                                scanner.nextLine();
+                                break;
+                            case 2:
+                                System.out.println("\n");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                EXPLICACION ELSE IF                *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *   En Java, else if se utiliza para manejar        *");
+                                System.out.println("        *   múltiples condiciones en una estructura de      *");
+                                System.out.println("        *   control de flujo después de una instrucción     *");
+                                System.out.println("        *   if inicial. Permite verificar varias            *");
+                                System.out.println("        *   condiciones secuenciales y ejecutar el bloque   *");
+                                System.out.println("        *   de código correspondiente a la primera          *");
+                                System.out.println("        *   condición que sea verdadera.                    *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *           Su estructura es la siguiente:          *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *    int x = 10;                                    *");
+                                System.out.println("        *    if (x > 15) {                                  *");
+                                System.out.println("        *        System.out.println(\"x es mayor que 15\"); *");
+                                System.out.println("        *    } else if (x > 5) {                            *");
+                                System.out.println("        *        System.out.println(\"x es mayor que 5      *");
+                                System.out.println("        *             pero menor o igual a 15\");           *");
+                                System.out.println("        *    } else if (x > 0) {                            *");
+                                System.out.println("        *        System.out.println(\"x es mayor que 0      *");
+                                System.out.println("        *             pero menor o igual a 5\");            *");
+                                System.out.println("        *    } else {                                       *");
+                                System.out.println("        *        System.out.println(\"x es menor o          *");
+                                System.out.println("        *             igual a 0\");                         *");
+                                System.out.println("        *    }                                              *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("\n Ingrese Enter para  volver al menu anterior");
+                                scanner.nextLine();
+                                scanner.nextLine();
+                                break;
+                            case 3:
+                                System.out.println("\n");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                  EXPLICACION ELSE                 *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *   En Java, el else se utiliza en una estructura   *");
+                                System.out.println("        *   de control de flujo if-else para proporcionar   *");
+                                System.out.println("        *   una opción alternativa cuando la condición      *");
+                                System.out.println("        *   del if no se cumple. Es una forma de manejar    *");
+                                System.out.println("        *   el caso en que ninguna de las condiciones       *");
+                                System.out.println("        *   previas es verdadera.                           *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *           Su estructura es la siguiente:          *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *    int x = 8;                                     *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *    if (x > 10) {                                  *");
+                                System.out.println("        *       System.out.println(\"x es mayor que 10\");  *");
+                                System.out.println("        *    } else {                                       *");
+                                System.out.println("        *       System.out.println(\"x es 10 o menos\");    *");
+                                System.out.println("        *    }                                              *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *****************************************************");
+                                System.out.println("\n Ingrese Enter para  volver al menu anterior");
+                                scanner.nextLine();
+                                scanner.nextLine();
+                                break;
+                            case 4:      /*Esta listo*/
+                                System.out.println("\n");
+                                System.out.println("        *****************************************************");
+                                System.out.println("        *               EJEMPLO IF, ELSE IF y ELSE          *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *                                                   *");
+                                System.out.println("        *****************************************************");
 
 
+                                // EXPLICACION DEL EJEMPLO IF, ELSE IF Y ELSE
 
+
+                                break;
+
+
+                        }
+                    }while (opcionSubMenu !=5);
                     break;
                 case 6:
 
@@ -509,10 +636,10 @@ public class Main {
                     do {
                         System.out.println("\n");
                         System.out.println("        *****************************************************");
-                        System.out.println("        *                  BUCLE DO WHILE                       *");
+                        System.out.println("        *                  BUCLE DO WHILE                   *");
                         System.out.println("        *****************************************************");
-                        System.out.println("        *       1.  Explicacion DO WHILE.                        *");
-                        System.out.println("        *       2.  Ejemplo DO WHILE8.                            *");
+                        System.out.println("        *       1.  Explicacion DO WHILE.                   *");
+                        System.out.println("        *       2.  Ejemplo DO WHILE8.                      *");
                         System.out.println("        *       3.  ATRAS                                   *");
                         System.out.println("        *                                                   *");
                         System.out.println("        *                                                   *");
@@ -546,7 +673,7 @@ public class Main {
                             case 2:      /*Esta listo*/
                                 System.out.println("\n");
                                 System.out.println("        *********************************************************");
-                                System.out.println("        *                      EJEMPLO DO WHILE                     *");
+                                System.out.println("        *                      EJEMPLO DO WHILE                 *");
                                 System.out.println("        *       Digite un numero  para realizar la sumatoria    *");
                                 System.out.println("        *             de los números enteros comprendidos       *");
                                 System.out.println("        *                 entre el 1 y el 10                    *");
