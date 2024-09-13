7941230c5db2adb297790599b241b4f5cb0380a1
yonnii12 <yonnii401@hotmail.com>
2024-09-13 17:01:57 -0500

    metodo menu

--- src/ejecucion/Main.java
+++ src/ejecucion/Main.java
@@ -1,5 +1,7 @@
 package ejecucion;
 
+import menu.MenuPrincipal;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,25 +14,7 @@ public class Main {
 
         do {
             // Mostrar menú principal
-            System.out.println("\n");
-            System.out.println("        *****************************************************");
-            System.out.println("        *       CAPITULO/GLOSARIO PROGRAMACION BASICA       *");
-            System.out.println("        *                 YVILL13-CMENES1                   *");
-            System.out.println("        *                   PRE-HACKATON                    *");
-            System.out.println("        *****************************************************");
-            System.out.println("        *       1.  Datos Primitivos.                       *");
-            System.out.println("        *       2.  String.                                 *");
-            System.out.println("        *       3.  Constantes.                             *");
-            System.out.println("        *       4.  Tipo de Operadores.                     *");
-            System.out.println("        *       5.  Condicional IF, ELSE IF Y ELSE.         *");
-            System.out.println("        *       6.  Condicional Switch.                     *");
-            System.out.println("        *       7.  Condicional Ternaria.                   *");
-            System.out.println("        *       8.  Bucle DO WHILE.                         *");
-            System.out.println("        *       9.  Bucle WHILE.                            *");
-            System.out.println("        *       10. Bucle FOR.                              *");
-            System.out.println("        *       0.  Salir.                                  *");
-            System.out.println("        *****************************************************");
-            System.out.print("\n Ingrese el número de la opción que desea ver: ");
+            MenuPrincipal.principalMEnu();
             opcionMenu = scanner.nextInt();
             System.out.println();
 
--- src/menu/MenuPrincipal.java
+++ src/menu/MenuPrincipal.java
@@ -1,4 +1,25 @@
 package menu;
 
 public class MenuPrincipal {
+    public static void principalMEnu(){
+        System.out.println("\n");
+        System.out.println("        *****************************************************");
+        System.out.println("        *       CAPITULO/GLOSARIO PROGRAMACION BASICA       *");
+        System.out.println("        *                 YVILL13-CMENES1                   *");
+        System.out.println("        *                   PRE-HACKATON                    *");
+        System.out.println("        *****************************************************");
+        System.out.println("        *       1.  Datos Primitivos.                       *");
+        System.out.println("        *       2.  String.                                 *");
+        System.out.println("        *       3.  Constantes.                             *");
+        System.out.println("        *       4.  Tipo de Operadores.                     *");
+        System.out.println("        *       5.  Condicional IF, ELSE IF Y ELSE.         *");
+        System.out.println("        *       6.  Condicional Switch.                     *");
+        System.out.println("        *       7.  Condicional Ternaria.                   *");
+        System.out.println("        *       8.  Bucle DO WHILE.                         *");
+        System.out.println("        *       9.  Bucle WHILE.                            *");
+        System.out.println("        *       10. Bucle FOR.                              *");
+        System.out.println("        *       0.  Salir.                                  *");
+        System.out.println("        *****************************************************");
+        System.out.print("\n Ingrese el número de la opción que desea ver: ");
+    }
 }
